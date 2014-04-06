17e260d88d482c51e1cbcd805bf7f5efb9726950
elopes42 <elopes06@gmail.com>
2014-04-06 01:16:07 -0300

    Arumado pequeno detalhe

--- TccTwitter/src/tcc/twitter/main/MainApplication.java
+++ TccTwitter/src/tcc/twitter/main/MainApplication.java
@@ -1,7 +1,18 @@
 package tcc.twitter.main;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.OAuth2Token;
+import twitter4j.auth.RequestToken;
+
 public class MainApplication {
-	public static void maint(String args[]) throws Exception{
+	public static void main(String args[]) throws Exception{
 	    // The factory instance is re-useable and thread safe.
 	    Twitter twitter = TwitterFactory.getSingleton();
 	    twitter.setOAuthConsumer("mpbw8VzN1xBO4Gefd0GSpg", "ehQnNojt7FUHLVQuGKmBlHDgo75z33ySKDEU8U");
